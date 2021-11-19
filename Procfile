12cfbb509ed0de2a2ec718c99130dc1cd0c99aa5
Stuart Quin <stuart.quin@gmail.com>
2021-11-19 16:27:10 +0000

    Procfile and gunicorn settings #1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir /app/ossso_app ossso.wsgi:application
--- ossso_app/ossso/settings.py
+++ ossso_app/ossso/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ossso.cloud.stuartquin.com",
+    "ossso-dev.cloud.stuartquin.com",
+    "localhost",
+]
 
 
 # Application definition
